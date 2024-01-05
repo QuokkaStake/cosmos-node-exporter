7bf39200c9aa1cdc2bdc0bd387bd78e2764355ae
Sergey <83376337+freak12techno@users.noreply.github.com>
2024-01-05 14:21:14 +0300

    feat: add start time metric (#27)

---
+++ pkg/queriers/uptime/uptime.go
@@ -0,0 +1,33 @@
+package uptime
+
+import (
+	"main/pkg/metrics"
+	"main/pkg/query_info"
+	"time"
+)
+
+type Querier struct {
+	StartTime time.Time
+}
+
+func NewQuerier() *Querier {
+	return &Querier{
+		StartTime: time.Now(),
+	}
+}
+
+func (u *Querier) Enabled() bool {
+	return true
+}
+
+func (u *Querier) Name() string {
+	return "uptime-querier"
+}
+
+func (u *Querier) Get() ([]metrics.MetricInfo, []query_info.QueryInfo) {
+	return []metrics.MetricInfo{{
+		MetricName: metrics.MetricNameStartTime,
+		Labels:     map[string]string{},
+		Value:      float64(u.StartTime.Unix()),
+	}}, []query_info.QueryInfo{}
+}
