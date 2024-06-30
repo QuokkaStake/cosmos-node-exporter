b10fea5e0dec43c2e2c3d652bc378cfc6778ec1f
Sergey <freak12techno@gmail.com>
2024-06-30 17:51:42 +0300

    chore: add grpc client test

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/BurntSushi/toml v1.2.1
 	github.com/Masterminds/semver v1.5.0
 	github.com/cosmos/cosmos-sdk v0.50.5
+	github.com/cosmos/gogoproto v1.4.11
 	github.com/creasty/defaults v1.7.0
 	github.com/google/uuid v1.6.0
 	github.com/jarcoal/httpmock v1.3.1
@@ -16,12 +17,13 @@ require (
 	github.com/rs/zerolog v1.32.0
 	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.9.0
+	go.nhat.io/grpcmock v0.26.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0
 	go.opentelemetry.io/otel v1.26.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.26.0
 	go.opentelemetry.io/otel/sdk v1.26.0
 	go.opentelemetry.io/otel/trace v1.26.0
-	google.golang.org/grpc v1.63.2
+	google.golang.org/grpc v1.64.0
 	gopkg.in/guregu/null.v4 v4.0.0
 )
 
@@ -42,6 +44,7 @@ require (
 	github.com/DataDog/zstd v1.5.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bgentry/speakeasy v0.1.1-0.20220910012023-760eaf8b6816 // indirect
+	github.com/bool64/shared v0.1.5 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
@@ -58,7 +61,6 @@ require (
 	github.com/cosmos/cosmos-proto v1.0.0-beta.4 // indirect
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/gogogateway v1.2.0 // indirect
-	github.com/cosmos/gogoproto v1.4.11 // indirect
 	github.com/cosmos/iavl v1.0.1 // indirect
 	github.com/cosmos/ics23/go v0.10.0 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.13.3 // indirect
@@ -105,6 +107,7 @@ require (
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/hdevalence/ed25519consensus v0.1.0 // indirect
 	github.com/huandu/skiplist v1.2.0 // indirect
+	github.com/iancoleman/orderedmap v0.3.0 // indirect
 	github.com/iancoleman/strcase v0.3.0 // indirect
 	github.com/improbable-eng/grpc-web v0.15.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -135,33 +138,39 @@ require (
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.18.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/swaggest/assertjson v1.9.0 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
 	github.com/tidwall/btree v1.7.0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	github.com/zondax/hid v0.9.2 // indirect
 	github.com/zondax/ledger-go v0.14.3 // indirect
 	go.etcd.io/bbolt v1.3.8 // indirect
+	go.nhat.io/matcher/v2 v2.0.0 // indirect
+	go.nhat.io/wait v0.1.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.26.0 // indirect
 	go.opentelemetry.io/otel/metric v1.26.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
-	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
-	golang.org/x/term v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/genproto v0.0.0-20240227224415-6ceb2ff114de // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240318140521-94a12d6c2237 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
---
+++ pkg/clients/grpc/client_test.go
@@ -0,0 +1,193 @@
+package grpc
+
+import (
+	"context"
+	configPkg "main/pkg/config"
+	loggerPkg "main/pkg/logger"
+	"main/pkg/tracing"
+	"testing"
+
+	"github.com/cometbft/cometbft/proto/tendermint/p2p"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.nhat.io/grpcmock/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"go.nhat.io/grpcmock"
+
+	cmtTypes "github.com/cosmos/cosmos-sdk/client/grpc/cmtservice"
+	nodeTypes "github.com/cosmos/cosmos-sdk/client/grpc/node"
+)
+
+func TestGrpcClientInit(t *testing.T) {
+	t.Parallel()
+
+	logger := loggerPkg.GetDefaultLogger()
+	tracer := tracing.InitNoopTracer()
+	config := configPkg.NodeConfig{GrpcConfig: configPkg.GrpcConfig{Address: "localhost:9090"}}
+	client := NewClient(config, *logger, tracer)
+	assert.NotNil(t, client)
+}
+func TestGrpcClientNodeConfigFail(t *testing.T) {
+	t.Parallel()
+
+	grpcmock.MockServer()
+
+	_, d := grpcmock.MockServerWithBufConn()(t)
+
+	logger := loggerPkg.GetDefaultLogger()
+	tracer := tracing.InitNoopTracer()
+	grpcConn, err := grpc.NewClient(
+		"invalid",
+		grpc.WithContextDialer(d),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	client := &Client{
+		Logger: *logger,
+		Client: grpcConn,
+		Tracer: tracer,
+	}
+
+	nodeConfig, queryInfo, err := client.GetNodeConfig(context.Background())
+	require.Error(t, err)
+	assert.False(t, queryInfo.Success)
+	assert.Nil(t, nodeConfig)
+}
+
+func TestGrpcClientNodeConfigNotImplemented(t *testing.T) {
+	t.Parallel()
+
+	grpcmock.MockServer()
+
+	_, d := grpcmock.MockServerWithBufConn()(t)
+
+	logger := loggerPkg.GetDefaultLogger()
+	tracer := tracing.InitNoopTracer()
+	grpcConn, err := grpc.NewClient(
+		"localhost:9090",
+		grpc.WithContextDialer(d),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	client := &Client{
+		Logger: *logger,
+		Client: grpcConn,
+		Tracer: tracer,
+	}
+
+	nodeConfig, queryInfo, err := client.GetNodeConfig(context.Background())
+	require.NoError(t, err)
+	assert.True(t, queryInfo.Success)
+	assert.Nil(t, nodeConfig)
+}
+
+func TestGrpcClientNodeConfigOk(t *testing.T) {
+	t.Parallel()
+
+	grpcmock.MockServer()
+
+	_, d := grpcmock.MockServerWithBufConn(
+		grpcmock.RegisterServiceFromMethods(service.Method{
+			ServiceName: "cosmos.base.node.v1beta1.Service",
+			MethodName:  "Config",
+			MethodType:  service.TypeUnary,
+			Input:       &nodeTypes.ConfigRequest{},
+			Output:      &nodeTypes.ConfigResponse{},
+		}),
+		func(s *grpcmock.Server) {
+			s.ExpectUnary("cosmos.base.node.v1beta1.Service/Config").
+				Return(&nodeTypes.ConfigResponse{MinimumGasPrice: "0.1uatom"})
+		},
+	)(t)
+
+	logger := loggerPkg.GetDefaultLogger()
+	tracer := tracing.InitNoopTracer()
+	grpcConn, err := grpc.NewClient(
+		"localhost:9090",
+		grpc.WithContextDialer(d),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	client := &Client{
+		Logger: *logger,
+		Client: grpcConn,
+		Tracer: tracer,
+	}
+
+	nodeConfig, queryInfo, err := client.GetNodeConfig(context.Background())
+	require.NoError(t, err)
+	assert.True(t, queryInfo.Success)
+	assert.NotNil(t, nodeConfig)
+	assert.Equal(t, "0.1uatom", nodeConfig.MinimumGasPrice)
+}
+
+func TestGrpcClientNodeInfoFail(t *testing.T) {
+	t.Parallel()
+
+	grpcmock.MockServer()
+
+	_, d := grpcmock.MockServerWithBufConn()(t)
+
+	logger := loggerPkg.GetDefaultLogger()
+	tracer := tracing.InitNoopTracer()
+	grpcConn, err := grpc.NewClient(
+		"invalid",
+		grpc.WithContextDialer(d),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	client := &Client{
+		Logger: *logger,
+		Client: grpcConn,
+		Tracer: tracer,
+	}
+
+	nodeConfig, queryInfo, err := client.GetNodeInfo(context.Background())
+	require.Error(t, err)
+	assert.False(t, queryInfo.Success)
+	assert.Nil(t, nodeConfig)
+}
+
+func TestGrpcClientNodeInfoOk(t *testing.T) {
+	t.Parallel()
+
+	grpcmock.MockServer()
+
+	_, d := grpcmock.MockServerWithBufConn(
+		grpcmock.RegisterServiceFromMethods(service.Method{
+			ServiceName: "cosmos.base.tendermint.v1beta1.Service",
+			MethodName:  "GetNodeInfo",
+			MethodType:  service.TypeUnary,
+			Input:       &cmtTypes.GetNodeInfoRequest{},
+			Output:      &cmtTypes.GetNodeInfoResponse{},
+		}),
+		func(s *grpcmock.Server) {
+			s.ExpectUnary("cosmos.base.tendermint.v1beta1.Service/GetNodeInfo").
+				Return(&cmtTypes.GetNodeInfoResponse{
+					DefaultNodeInfo: &p2p.DefaultNodeInfo{Network: "chain"},
+				})
+		},
+	)(t)
+
+	logger := loggerPkg.GetDefaultLogger()
+	tracer := tracing.InitNoopTracer()
+	grpcConn, err := grpc.NewClient(
+		"localhost:9090",
+		grpc.WithContextDialer(d),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	client := &Client{
+		Logger: *logger,
+		Client: grpcConn,
+		Tracer: tracer,
+	}
+
+	nodeInfo, queryInfo, err := client.GetNodeInfo(context.Background())
+	require.NoError(t, err)
+	assert.True(t, queryInfo.Success)
+	assert.NotNil(t, nodeInfo)
+	assert.Equal(t, "chain", nodeInfo.DefaultNodeInfo.Network)
+}
