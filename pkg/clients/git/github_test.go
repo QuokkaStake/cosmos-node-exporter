2e17036da9c013eea05618743d6474bbc5464ec3
Sergey <83376337+freak12techno@users.noreply.github.com>
2024-08-21 16:02:12 +0300

    feat: cache GitHub queries (#57)

--- pkg/clients/git/github_test.go
+++ pkg/clients/git/github_test.go
@@ -7,7 +7,9 @@ import (
 	configPkg "main/pkg/config"
 	loggerPkg "main/pkg/logger"
 	"main/pkg/tracing"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
@@ -62,7 +64,9 @@ func TestGetGithubClientInvalidResponse(t *testing.T) {
 	httpmock.RegisterResponder(
 		"GET",
 		"https://api.github.com/repos/cosmos/gaia/releases/latest",
-		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("invalid.toml")),
+		httpmock.
+			NewBytesResponder(200, assets.GetBytesOrPanic("invalid.toml")).
+			HeaderAdd(http.Header{"x-ratelimit-reset": []string{"12345"}}),
 	)
 
 	config := configPkg.GitConfig{Repository: "https://github.com/cosmos/gaia"}
@@ -79,6 +83,32 @@ func TestGetGithubClientInvalidResponse(t *testing.T) {
 }
 
 //nolint:paralleltest // disabled due to httpmock usage
+func TestGetGithubClientInvalidRatelimitHeader(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		"https://api.github.com/repos/cosmos/gaia/releases/latest",
+		httpmock.
+			NewBytesResponder(200, assets.GetBytesOrPanic("invalid.toml")).
+			HeaderAdd(http.Header{"x-ratelimit-reset": []string{"asd"}}),
+	)
+
+	config := configPkg.GitConfig{Repository: "https://github.com/cosmos/gaia", Token: "aaa"}
+	logger := loggerPkg.GetNopLogger()
+	tracer := tracing.InitNoopTracer()
+	client := NewGithub(config, *logger, tracer)
+
+	release, queryInfo, err := client.GetLatestRelease(context.Background())
+
+	require.Error(t, err)
+	require.ErrorContains(t, err, "invalid syntax")
+	assert.False(t, queryInfo.Success)
+	require.Empty(t, release)
+}
+
+//nolint:paralleltest // disabled due to httpmock usage
 func TestGetGithubClientRateLimit(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -86,7 +116,9 @@ func TestGetGithubClientRateLimit(t *testing.T) {
 	httpmock.RegisterResponder(
 		"GET",
 		"https://api.github.com/repos/cosmos/gaia/releases/latest",
-		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("github-error.json")),
+		httpmock.
+			NewBytesResponder(200, assets.GetBytesOrPanic("github-error.json")).
+			HeaderAdd(http.Header{"x-ratelimit-reset": []string{"12345"}}),
 	)
 
 	config := configPkg.GitConfig{Repository: "https://github.com/cosmos/gaia"}
@@ -110,30 +142,9 @@ func TestGetGithubClientValidQueried(t *testing.T) {
 	httpmock.RegisterResponder(
 		"GET",
 		"https://api.github.com/repos/cosmos/gaia/releases/latest",
-		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("github-valid.json")),
-	)
-
-	config := configPkg.GitConfig{Repository: "https://github.com/cosmos/gaia"}
-	logger := loggerPkg.GetNopLogger()
-	tracer := tracing.InitNoopTracer()
-	client := NewGithub(config, *logger, tracer)
-
-	release, queryInfo, err := client.GetLatestRelease(context.Background())
-
-	require.NoError(t, err)
-	assert.True(t, queryInfo.Success)
-	require.Equal(t, "v17.2.0", release)
-}
-
-//nolint:paralleltest // disabled due to httpmock usage
-func TestGetGithubClientCachedNoPreviousResponse(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder(
-		"GET",
-		"https://api.github.com/repos/cosmos/gaia/releases/latest",
-		httpmock.NewBytesResponder(304, assets.GetBytesOrPanic("github-valid.json")),
+		httpmock.
+			NewBytesResponder(200, assets.GetBytesOrPanic("github-valid.json")).
+			HeaderAdd(http.Header{"x-ratelimit-reset": []string{"12345"}}),
 	)
 
 	config := configPkg.GitConfig{Repository: "https://github.com/cosmos/gaia"}
@@ -153,17 +164,15 @@ func TestGetGithubClientCachedWithPreviousResponse(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	httpmock.RegisterResponder(
-		"GET",
-		"https://api.github.com/repos/cosmos/gaia/releases/latest",
-		httpmock.NewBytesResponder(304, assets.GetBytesOrPanic("github-valid.json")),
-	)
-
-	config := configPkg.GitConfig{Repository: "https://github.com/cosmos/gaia", Token: "aaa"}
+	config := configPkg.GitConfig{
+		Repository: "https://github.com/cosmos/gaia",
+		Token:      "aaa",
+	}
 	logger := loggerPkg.GetNopLogger()
 	tracer := tracing.InitNoopTracer()
 	client := NewGithub(config, *logger, tracer)
 	client.LastResult = "v1.2.3"
+	client.LastResultTime = time.Now()
 
 	release, queryInfo, err := client.GetLatestRelease(context.Background())
 
